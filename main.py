a55a9d9f1af6eaa3d3e4492b3e7f8339c6d35c86
Nemexia <amirhossein.rahmani55@gmail.com>
2024-09-02 20:09:37 +0330

    Add main_page choice and actions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import version
 import models
+import version
 
 
 class Application:
@@ -7,13 +7,24 @@ class Application:
         self.inventory = models.Inventory()
 
     def run(self):
-        self.main_page()
+        while True:
+            choice = self.main_page()
+            if choice == "1":
+                pass
+            if choice == "2":
+                pass
+            if choice == "3":
+                exit()
 
-    def main_page(self):
+    def main_page(self) -> str:
         print(f"Nemexia's Inventory Manager v{version.version}")
+        print("")
         print("choose option:")
         print("1: New Inventory")
         print("2: Load Inventory")
+        print("3: Exit")
+        choice = input(">> ")
+        return choice
 
 
 if __name__ == '__main__':
