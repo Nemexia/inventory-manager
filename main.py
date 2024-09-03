a28eab3efde11ff4abb2eb30ce5704bff1f09f78
Nemexia <amirhossein.rahmani55@gmail.com>
2024-09-03 17:54:22 +0330

    Add new, load and exit methods for Application

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import version
 
 class Application:
     def __init__(self):
-        self.inventory = models.Inventory()
+        self.inventory:models.Inventory|None = None
 
     def run(self):
         while True:
             choice = self.main_page()
             if choice == "1":
-                pass
+                self.new_inventory()
             if choice == "2":
-                pass
+                self.load_inventory()
             if choice == "3":
-                exit()
+                self.exit()
 
     def main_page(self) -> str:
         print(f"Nemexia's Inventory Manager v{version.version}")
@@ -25,6 +25,22 @@ class Application:
         print("3: Exit")
         choice = input(">> ")
         return choice
+    
+    def new_inventory(self):
+        print("New Inventory")
+        print("Enter the name of the inventory:")
+        name = input(">> ")
+        self.inventory = models.Inventory(name)
+    
+    def load_inventory(self):
+        print("Load Inventory")
+        print("Enter the name of the inventory:")
+        name = input(">> ")
+        self.inventory = models.Inventory(name)
+
+    def exit(self):
+        print("Exiting...")
+        exit()
 
 
 if __name__ == '__main__':
